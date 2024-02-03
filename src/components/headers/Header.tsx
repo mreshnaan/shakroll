0754360f00849f7171a19d8cd8fa1fdb6197af72
unknown <mreshnaan7@gmail.com>
2024-02-03 12:22:11 +0530

    add mobile menu

--- src/components/headers/Header.tsx
+++ src/components/headers/Header.tsx
@@ -1,4 +1,5 @@
 import { Disclosure } from '@headlessui/react'
+import Divider from '../Divider'
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
@@ -76,25 +77,33 @@ export default function Header() {
             </div>
           </div>
 
-          <Disclosure.Panel className="lg:hidden h-screen w-screen relative">
-          <img className=" absolute lg:hidden top-[0px] h-screen w-screen right-0 z-[-1]" src="/assets/ellipse5.svg"  loading='lazy'/>
+          <Disclosure.Panel className="lg:hidden h-screen w-screen relative flex flex-col gap-[40px]">
+            <img className=" absolute lg:hidden top-[0px] h-screen w-screen right-0 z-[-1]" src="/assets/ellipse5.svg" loading='lazy' />
 
-            <div className="space-y-1 px-2 pb-3 pt-2">
+            <div className="space-y-1 px-2 pb-3 pt-12 items-start  ">
               {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current ? 'text-white' : 'text-gray-300 hover:text-white',
-                    'block rounded-md px-3 py-2 text-base font-medium'
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
+                <>
+                  <Disclosure.Button
+                    key={item.name}
+                    as="a"
+                    href={item.href}
+                    className={classNames(
+                      item.current ? 'text-white' : 'text-gray-300 hover:text-[#69EAFF]',
+                      'flex items-center gap-5 px-3 py-4 text-base font-medium'
+                    )}
+                    aria-current={item.current ? 'page' : undefined}
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                      <path d="M15.7071 8.70711C16.0976 8.31658 16.0976 7.68342 15.7071 7.29289L9.34315 0.928933C8.95262 0.538409 8.31946 0.538409 7.92893 0.928933C7.53841 1.31946 7.53841 1.95262 7.92893 2.34315L13.5858 8L7.92893 13.6569C7.53841 14.0474 7.53841 14.6805 7.92893 15.0711C8.31946 15.4616 8.95262 15.4616 9.34315 15.0711L15.7071 8.70711ZM-8.74228e-08 9L15 9L15 7L8.74228e-08 7L-8.74228e-08 9Z" fill="currentColor" />
+                    </svg>
+                    {item.name}
+                  </Disclosure.Button>
+                  <Divider />
+                </>
               ))}
+
             </div>
+            <button className='button-gradient-1  py-3 px-8 rounded-[6px] w-[230px] text-[#FFF] text-[19px] mx-auto leading-7 uppercase mt-7'>JOIN COMMUNITY</button>
           </Disclosure.Panel>
         </>
       )
