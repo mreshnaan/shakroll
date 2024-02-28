efa19a71ece604fd669302bf9ccd10dd139491b2
LahiruLakshan <balahirulakshan@gmail.com>
2024-02-28 12:34:11 +0530

    scroll issue fixed

--- src/components/roadMap/RoadMap.tsx
+++ src/components/roadMap/RoadMap.tsx
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import React, { Dispatch, SetStateAction } from "react";
 import DetailsButton from "./DetailsButton";
 import { steps } from "./dates";
-import { useScrollDirection } from "../useScrollDirection";
 
 interface TimelineStepProps {
   date: string;
@@ -24,24 +23,23 @@ const TimelineStep: React.FC<TimelineStepProps> = ({ actionLabels }) => {
   );
 };
 
-const Timeline: React.FC = () => {
-  const [activeStep, setActiveStep] = useState<number>(0);
+const Timeline: React.FC<{ activeStep: any; setActiveStep: Dispatch<SetStateAction<number>> }> = ({activeStep, setActiveStep}) => {
+  // const [activeStep, setActiveStep] = useState<number>(0);
   const progress = (activeStep / (steps.length - 1)) * 100;
-  const scrollDirection = useScrollDirection();
   const handleClick = (index: number) => {
     setActiveStep(index);
     console.log(activeStep);
   };
 
-  const handleScroll = () => {
-    console.log("scrollDirection : ", scrollDirection);
-    if (scrollDirection === 'up' && activeStep >= 1) {
-      setActiveStep(activeStep - 1);
-    } else if (scrollDirection === 'down' && activeStep <= 3) {
-      setActiveStep(activeStep + 1);
-    }
+  // const handleScroll = () => {
+  //   console.log("scrollDirection : ", scrollDirection);
+  //   if (scrollDirection === 'up' && activeStep >= 1) {
+  //     setActiveStep(activeStep - 1);
+  //   } else if (scrollDirection === 'down' && activeStep <= 3) {
+  //     setActiveStep(activeStep + 1);
+  //   }
 
-  };
+  // };
 
   return (
     <div className="hidden lg:flex">
@@ -104,8 +102,8 @@ const Timeline: React.FC = () => {
           </div>
         </div>
 
-        <div className="flex justify-center flex-grow col-span-6 overflow-auto" onWheel={handleScroll}
-      onTouchMove={handleScroll}>
+        <div className="flex justify-center flex-grow col-span-6 overflow-auto"
+      >
           {activeStep !== null && steps[activeStep] && (
             <TimelineStep key={activeStep} {...steps[activeStep]} />
           )}
