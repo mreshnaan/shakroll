9beecfcd1818558f3b621a6f760efd69e5987899
Rikas <rikasrkf@gmail.com>
2024-03-05 10:02:20 +0530

    google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,35 @@
-import Banner from "./components/Banner"
-import Logobar from "./components/Logobars"
-import Section2 from "./components/Section2"
-import Section3 from "./components/Section3"
-import Section5 from "./components/Section5"
-import Section6 from "./components/Section6"
-import Section7 from "./components/Section7"
-import Section8 from "./components/Section8"
-import Section9 from "./components/Section9"
-import Footer from "./components/footers/Footer"
-import Header from "./components/headers/Header"
-
+import Banner from "./components/Banner";
+import Logobar from "./components/Logobars";
+import Section2 from "./components/Section2";
+import Section3 from "./components/Section3";
+import Section5 from "./components/Section5";
+import Section6 from "./components/Section6";
+import Section7 from "./components/Section7";
+import Section8 from "./components/Section8";
+import Section9 from "./components/Section9";
+import Footer from "./components/footers/Footer";
+import Header from "./components/headers/Header";
+import GoogleAnalytics from "./lib/GoogleAnalytics";
 
 function App() {
-
-
   return (
-    <div className='w-full min-h-screen'>
-      <Header />
-      <Banner />
-      <Logobar />
-      <Section2 />
-      <Section3 />
-      <Section5 />
-      <Section6 />
-      <Section7 />
-      <Section8 />
-      <Section9 />
-      <Footer />
-
-
-    </div>
-  )
+    <>
+      <GoogleAnalytics />
+      <div className="w-full min-h-screen">
+        <Header />
+        <Banner />
+        <Logobar />
+        <Section2 />
+        <Section3 />
+        <Section5 />
+        <Section6 />
+        <Section7 />
+        <Section8 />
+        <Section9 />
+        <Footer />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
